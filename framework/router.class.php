8c4aecb701550794a3cbb77954a8c78db21cf336
caoyanyi <caoyanyi@easycorp.ltd>
2022-05-13 22:48:20 +0800

    * Modify version style.

--- framework/router.class.php
+++ framework/router.class.php
@@ -878,7 +878,7 @@ class router
     public function parseRequest()
     {
         global $argv;
-        if(in_array($argv[1], array('-v', '--version'))) die(fwrite(STDOUT, "z {$this->config->version}". PHP_EOL));
+        if(in_array($argv[1], array('-v', '--version'))) die(fwrite(STDOUT, $this->config->version . PHP_EOL));
 
         $module = (empty($argv[1]) or substr($argv[1], 0, 1) == '-' or $argv[1] == 'help') ? $this->config->default->module : $argv[1];
         $method = (empty($argv[2]) or substr($argv[2], 0, 1) == '-') ? $this->config->default->method : $argv[2];
