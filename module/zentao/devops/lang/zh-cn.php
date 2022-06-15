8ac403cc76da2aa76bd6a5f45ea755e69b7d41ff
liyuchun <563917701@qq.com>
2022-06-15 07:09:33 +0000

    * Finish task #57070.

--- module/zentao/devops/lang/zh-cn.php
+++ module/zentao/devops/lang/zh-cn.php
@@ -28,11 +28,15 @@ $lang->devops->help->mr = <<<EOF
  z mr master
  z devops mr master
 EOF;
-$lang->devops->urlTip      = '请输入禅道地址： 例如：https://zentaopms.com：';
-$lang->devops->accountTip  = '用户名：';
-$lang->devops->pwdTip      = '密码：';
-$lang->devops->checking    = '正在检验网址...';
-$lang->devops->logging     = '正在登录...';
-$lang->devops->urlInvalid  = '该网址 %s 不存在或不是禅道网址，请重新输入：';
-$lang->devops->loginFailed = '登录失败，请验证账号密码后重试：';
-$lang->devops->loginLimit  = '密码尝试次数太多，请联系管理员解锁，或10分钟后重试。';
+
+$lang->devops->urlTip         = '请输入禅道地址： 例如：https://zentaopms.com：';
+$lang->devops->accountTip     = '用户名：';
+$lang->devops->pwdTip         = '密码：';
+$lang->devops->checking       = '正在检验网址...';
+$lang->devops->logging        = '正在登录...';
+$lang->devops->urlInvalid     = '该网址 %s 不存在或不是禅道网址，请重新输入：';
+$lang->devops->loginFailed    = '登录失败，请验证账号密码后重试：';
+$lang->devops->loginLimit     = '密码尝试次数太多，请联系管理员解锁，或10分钟后重试。';
+$lang->devops->notRepository  = '当前目录不是一个版本库';
+$lang->devops->noTracking     = '当前分支还未提交到远程。';
+$lang->devops->noTargetBranch = '目标分支不是一个有效分支。';
