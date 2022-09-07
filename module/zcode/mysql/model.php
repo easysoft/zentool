67305770c9078bb79a38d20ba916d3cb33f66c3f
Guan Xiying <guanxiying@easycorp.ltd>
2022-09-07 11:05:21 +0800

    * Adjust create config from fields function.

--- module/zcode/mysql/model.php
+++ module/zcode/mysql/model.php
@@ -39,6 +39,29 @@ class mysqlModel extends model
     public function parseConfigByTable($table)
     {
         $fields = $this->dbh->query("desc $table")->fetchAll();
+
+        $config = '';
+        $configTypes = array('common', 'action', 'required', 'search');
+
+        foreach($configTypes as $type)
+        {
+            $config .= "/* Create by zcode : $type items start. */\n";
+            if(is_callable(array($this, "parse{$type}Config")))$config .= call_user_func(array($this, "parse{$type}Config"), $fields);
+            $config .= "/* Create by zcode : $type items end. */\n\n";
+        }
+
+        return $config;
+    }
+
+    /**
+     * Parse common config.
+     *
+     * @param  array    $fields
+     * @access public
+     * @return string
+     */
+    public function parseCommonConfig($fields)
+    {
         $config = '';
         foreach($fields as $field)
         {
@@ -58,6 +81,45 @@ class mysqlModel extends model
     }
 
     /**
+     * Parse action config from fields.
+     *
+     * @param  array    $fields
+     * @access public
+     * @return string
+     */
+    public function parseActionConfig($fields)
+    {
+        $actions = array('view', 'create');
+        $config = '';
+
+        foreach($actions as $action)
+        {
+            $config .= "\$config->actions = new stdclass;\n";
+            $config .= "/* Items for {$action} action .*/\n";
+            $config .= "\$config->actions->{$action} = array();\n";
+            foreach($fields as $field)
+            {
+                $control = 'input';
+                if(strpos($field->Type, 'enum') !== false) $control = 'select';
+                if(strpos($field->Type, 'date') !== false) $control = 'date';
+                if(strpos($field->Type, 'text') !== false) $control = 'text';
+
+                $config .= "\$config->actions->{$action}['{$field->Field}']['name']         = '$field->Field';\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['label']        = '$field->Field';\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['control']      = '$control';\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['options']      = '';\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['show']         = 1;\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['width']        = 'auto';\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['position']     = 1;\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['defaultValue'] = '';\n\n";
+                $config .= "\$config->actions->{$action}['{$field->Field}']['rules']        = '';\n\n";
+            }
+            $config .= "/* Items for {$action} end.*/\n";
+        }
+        return $config;
+    }
+
+    /**
      * Check user input.
      *
      * @param  string $field
@@ -73,14 +135,19 @@ class mysqlModel extends model
         return true;
     }
 
-    public function parseLang($fields)
+    public function parseLang($table)
     {
-        return $fields;
+        $fields = $this->getFields($table);
     }
 
     public function getFormConfig($fields)
     {
         return $fields;
+    }
+
+    public function getFields($table)
+    {
+        return  $this->dbh->query("desc $table")->fetchAll();
     }
 }
 
