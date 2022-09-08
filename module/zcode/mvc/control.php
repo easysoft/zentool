62b216a30b2d220317f400264703757bf3d469f5
zhouxin <zhouxin@easycorp.ltd>
2022-09-08 16:55:50 +0800

    * Refactor code for resources method of mvc module.

--- module/zcode/mvc/control.php
+++ module/zcode/mvc/control.php
@@ -3,13 +3,15 @@ class mvc extends control
 {
     public function resources($module, $resourceFile = '')
     {
-        $controlFile  = $this->config->runDir . DS . $module . DS . 'control.php';
+        $controlFile  = $this->config->pmsRoot . 'module' . DS . $module . DS . 'control.php';
+        if(empty($resourceFile)) $resourceFile = $this->config->pmsRoot . 'module/group/lang/resource.php';
         $className = $module;
         $methods = $this->zcode->getMethodsByClass($controlFile, $className);
         $resources = "\$lang->resource->$module = new stdclass();\n";
         foreach($methods as $method)
         {
-            $resources = "\$lang->resource->$module->$method = '$method';\n";
+            if($method == '__construct') continue;
+            $resources .= "\$lang->resource->$module->$method = '$method';\n";
         }
 
         file_put_contents($resourceFile, $resources, FILE_APPEND);
